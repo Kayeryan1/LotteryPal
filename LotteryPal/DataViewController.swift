8920591e4f8b816712b24bb74aa0432d779feb8b
Ryan Kaye <Ryan@dhcp-10-134-202-224.uawifi.arizona.edu>
2016-01-23 11:54:28 -0700

    updated labels for goal

--- LotteryPal/DataViewController.swift
+++ LotteryPal/DataViewController.swift
@@ -14,8 +14,15 @@ class DataViewController: UIViewController {
     @IBOutlet weak var dataLabel: UILabel!
     
     var dataObject: String = ""
+    var goal: String = "0";
+    var jackpot: Int? = 100
 
     @IBOutlet weak var editGoalButton: UIButton!
+    @IBOutlet weak var goalConfirmButton: UIButton!
+    @IBOutlet weak var goalUpdateField: UITextField!
+    @IBOutlet weak var goalExceededLabel: UILabel!
+    @IBOutlet weak var goalLabel: UILabel!
+    
 
     @IBAction func editGoal(sender: AnyObject) {
         editGoalView.hidden = false;
@@ -24,9 +31,24 @@ class DataViewController: UIViewController {
         super.viewDidLoad()
         
         // initially hide editGoalView
-        editGoalView.hidden = true;
-        
+        editGoalView.hidden = true
+        goalExceededLabel.hidden = true
+        goalUpdateField.text=goal
+        goalLabel.text = "Goal: $" + goal + "mil";
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    //when you click the button to confirm your goal
+    @IBAction func confirmGoal(sender: AnyObject) {
+        editGoalView.hidden=true;
+        
+        if (goalUpdateField.text != nil){
+            goal = goalUpdateField.text!
+        }
+        if (Int(goal)>=jackpot){
+            goalExceededLabel.hidden = false;
+        }
+        goalLabel.text = "Goal: $" + goal + "mil";        
     }
 
     override func didReceiveMemoryWarning() {
