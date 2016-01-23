d10f00ff153092ddd512a30ca202f2c9e873b95d
Sarina White <sarinarw@gmail.com>
2016-01-23 10:40:56 -0700

    button makes edit goal area pop up

--- LotteryPal/DataViewController.swift
+++ LotteryPal/DataViewController.swift
@@ -10,12 +10,22 @@ import UIKit
 
 class DataViewController: UIViewController {
 
+    @IBOutlet weak var editGoalView: UIView!
     @IBOutlet weak var dataLabel: UILabel!
+    
     var dataObject: String = ""
 
+    @IBOutlet weak var editGoalButton: UIButton!
 
+    @IBAction func editGoal(sender: AnyObject) {
+        editGoalView.hidden = false;
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // initially hide editGoalView
+        editGoalView.hidden = true;
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
